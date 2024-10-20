31c6c3714a51c35217dbe55efb4944e19c25e16e
Harshini <143423201+harshini-pokuru@users.noreply.github.com>
2024-10-20 23:06:17 +0530

    Add files via upload

---
+++ Numbers/GreatestOfThree.java
@@ -0,0 +1,14 @@
+package Numbers;
+import java.util.*;
+public class GreatestOfThree {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(Math.max(a,Math.max(b,c)));
+
+        sc.close();
+    }
+}
